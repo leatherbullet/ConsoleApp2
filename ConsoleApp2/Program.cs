3fd9260e2086a81dfe09cd8b7f26da6dc22b34d7
leatherbullet <al.obnizov@yandex.ru>
2024-03-07 12:18:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[,] array = new int[2, 3];
+
+            int minValue = 1;
+            int maxValue = 10;
+            int lineArraySum = 0;
+            int columArrayMultiple = 1;
+            int lineArray = 1;
+            int columArray = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minValue, maxValue);
+                    Console.Write(array[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                lineArraySum += array[lineArray, i];
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                columArrayMultiple *= array[i, columArray];
+            }
+
+            Console.WriteLine($"Сумма строки {lineArray + 1} равна:{lineArraySum}\n" +
+                $"Произведение солбца {columArray + 1} равен:{columArrayMultiple}");
+        }
+    }
+}
